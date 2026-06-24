6c998c490a753e6c3059c03e1a8ccd655af59d6b
annemutahi <ms.nyamburamutahi@gmail.com>
2026-06-24 10:29:31 +0300

    added allowed hosts list

--- backend/settings.py
+++ backend/settings.py
@@ -26,12 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.getenv('DEGUG')
-DEBUG = True
+DEBUG = os.getenv('DEGUG')
 
-
-ALLOWED_HOSTS = ['127.0.0.1']
-# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
